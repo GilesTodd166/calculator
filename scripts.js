55094f4bf36a5bb661877867f03b17867c2071aa
gilestodd166 <167215806+GilesTodd166@users.noreply.github.com>
2025-05-03 13:18:04 -0400

    Rewrote functionality for calculator to simplify.
    operatorClicked used to track changes and allow for multiple calculations to be handled in one go.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,32 @@
 // Basic math functions, add, subtract, divide, multiply.
 
-function add (a, b) {
+function add(a, b) {
     return a + b;
 };
 
-function subtract (a, b) {
+function subtract(a, b) {
     return a - b;
 };
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 };
 
-function multiply (a, b) {
+function multiply(a, b) {
     return a * b;
 };
 
-// Create function to populate display on digit click. Store display number in var.
-
-let firstNum = '';
-let secondNum = '';
-let operatorChoice = '';
+function operate(firstNum, secondNum, operatorChoice) {
+    if (operatorChoice == '+') {
+        result = add(firstNum, secondNum);
+    } else if (operatorChoice == '-') {
+        result = subtract(firstNum, secondNum); 
+    } else if (operatorChoice == 'x') {
+        result = multiply(firstNum, secondNum); 
+    } else {
+        result = divide(firstNum, secondNum);
+    }
+};
 
 let numberButtons = document.querySelectorAll('.number-btn');
 let displayNum = document.getElementById('display');
@@ -30,89 +36,54 @@ let operatorButtons = document.querySelectorAll('.operator-btn');
 let equalsButton = document.querySelector('.equals-btn');
 
 let clearButton = document.querySelector('.clear-btn');
-let delButton = document.querySelector('.del-btn');
 
-let number = '';
-    function operate (a, b, mathOp) {
-        let aToInt = parseInt(a);
-        let bToInt = parseInt(b);
-            if (mathOp == '+') {
-                number = (aToInt + bToInt);
-            } else if (mathOp == '-') { 
-                number = (a - b);
-            } else if (mathOp == '/') {
-                number = (a / b);
-            } else {
-                number = (a * b);
-            }
-            return number;
-    };
+let currentDisplay = '';
+let firstNum = '';
+let secondNum = '';
+let operatorChoice = '';
+let operatorClicked = false;
+let result = '';
 
 numberButtons.forEach((number) => {
-    number.addEventListener('click', (e) => {                                                                  
-                if (operatorChoice == '') {
-                    firstNum = displayNum.innerHTML += (e.target.getAttribute('data-num'));
-                } else if (operatorChoice != '') {
-                    secondNum += (e.target.getAttribute('data-num'));
-                    displayNum.innerHTML = secondNum;
+    number.addEventListener('click', (e) => {
+        displayNum.innerHTML += (e.target.getAttribute('data-num'));
+                if (operatorClicked) {
+                    secondNum = currentDisplay = parseInt(displayNum.innerHTML);
+                } else {
+                    firstNum = currentDisplay = parseInt(displayNum.innerHTML);
                 }
     });
 });
 
-operatorButtons.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        operatorChoice = (e.target.getAttribute('data-ref'));
-        displayNum.innerHTML = operatorChoice;
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+        displayNum.innerHTML = currentDisplay = ''; // Would like to remove this, leave firstNum on display
+            if (firstNum != '' && operatorChoice != '' && secondNum != '') {
+                operate(firstNum, secondNum, operatorChoice);
+                firstNum = result;
+                operatorChoice = (e.target.getAttribute('data-ref'));
+            } else {
+                operatorChoice = (e.target.getAttribute('data-ref'));
+            };
+            operatorClicked = true;
     });
 });
 
-equalsButton.addEventListener('click', (e) => {
-    if (secondNum == '') {
-        equalsButton.removeEventListener('click', e);
+equalsButton.addEventListener('click', () => {
+    if (firstNum == '' || operatorChoice == '' || secondNum == '') {
+        return;
     } else {
-    operate(firstNum, secondNum, operatorChoice);
-    displayNum.innerHTML = number;
+        operate(firstNum, secondNum, operatorChoice);
+        displayNum.innerHTML = result;
     };
 });
 
 clearButton.addEventListener('click', () => {
-    displayNum.innerHTML = '';
     firstNum = '';
     secondNum = '';
     operatorChoice = '';
+    result = '';
+    currentDisplay = '';
+    displayNum.innerHTML = '';
+    operatorClicked = false;
 });
-
-// let updatedNum;
-// delButton.addEventListener('click', () => {
-//         if (secondNum == '') { 
-//             updatedNum = Math.floor(firstNum / 10);
-//             displayNum.innerHTML = updatedNum;
-//             firstNum = updatedNum;
-//         } else {
-//             updatedNum = Math.floor(secondNum / 10);
-//             displayNum.innerHTML = updatedNum;
-//             secondNum = updatedNum;
-//         }
-//             if (firstNum === 0 || secondNum ===  0) {
-//                 displayNum.innerHTML = '';
-//             }
-// });
-
-// // Create function to store first number choice, 
-// // operation choice and second number choice, 
-// // pass to operate function on equal press.
-
-// function handleOperate (firstNum, secondNum, operatorChoice) {
-//     if (firstNum != '' &&  secondNum != '' && operatorChoice != '') {
-//         operate(firstNum, secondNum, operatorChoice);
-//         number = displayNum.innerHTML;
-//     }
-// };
-
-// console.log(firstNum);
-// console.log(operatorChoice);
-// console.log(secondNum);
-// console.log(number);
-
-// console.log(firstNum);
-// console.log(secondNum);
